9df69f5793afea5d028fe837910b5d2f5af68a30
ryber <ryan.bergman@gmail.com>
2014-05-11 10:15:12 -0500

    exception throwing

--- src/test/java/examples/ObjectLifeCycle.java
+++ src/test/java/examples/ObjectLifeCycle.java
@@ -3,10 +3,13 @@ package examples;
 import junit.framework.Assert;
 import org.junit.Test;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.function.Supplier;
 
 import static junit.framework.Assert.assertNotSame;
 import static junit.framework.Assert.assertSame;
+import static junit.framework.Assert.fail;
 
 public class ObjectLifeCycle {
 
@@ -35,6 +38,19 @@ public class ObjectLifeCycle {
         assertSame(foo, foo2);
     }
 
+    @Test
+    public void lambdasStackTraceIsStillNice(){
+        LambdaFactory factory = new LambdaFactory();
+        Supplier foo = factory.getThrowingLambda();
+
+        try {
+            foo.get();
+            fail("Should have thrown");
+        }catch (Exception e){
+            Assert.assertTrue(getStackTrace(e).contains("at examples.ObjectLifeCycle$LambdaFactory.lambda$1(ObjectLifeCycle.java:"));
+        }
+    }
+
     public static class LambdaFactory {
         public Supplier<Integer> getLambdaSupplier(){
             return () -> 42;
@@ -48,5 +64,16 @@ public class ObjectLifeCycle {
                 }
             };
         }
+
+        public Supplier<Integer> getThrowingLambda(){
+             return () -> {throw new RuntimeException();};
+        }
     }
+
+    private String getStackTrace(Exception e) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+
 }
