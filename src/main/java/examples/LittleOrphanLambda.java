b348bdf3a6571531632ca2362fa378599a7c8378
ryber <ryan.bergman@gmail.com>
2014-05-12 22:12:38 -0500

    clean up the orphan a little

--- src/main/java/examples/LittleOrphanLambda.java
+++ src/main/java/examples/LittleOrphanLambda.java
@@ -4,15 +4,17 @@ import java.lang.invoke.*;
 
 public class LittleOrphanLambda {
     public static void main(String[] args) throws Throwable {
+
         MethodHandles.Lookup me= MethodHandles.lookup();
         MethodType t = MethodType.methodType(void.class);
         MethodType rt = MethodType.methodType(Runnable.class);
+
         CallSite site = LambdaMetafactory.metafactory(
                 me, "run", rt, t, me.findStatic(LittleOrphanLambda.class, "sayHello", t), t);
-        MethodHandle factory=site.getTarget();
-        Runnable r=(Runnable)factory.invoke();
 
-        System.out.println("created lambda: "+r);
+        MethodHandle factory = site.getTarget();
+        Runnable r = (Runnable)factory.invoke();
+
         r.run();
     }
     private static void sayHello() {
